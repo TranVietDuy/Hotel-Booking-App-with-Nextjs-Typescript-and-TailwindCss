a72216cf2f94b975aa47c93080a7c6163841a659
Anton Sydorenko <antonsydorenko@Antons-MacBook-Air.local>
2022-09-16 11:45:15 +0300

    added light theme colors to the tailwind.config file, deleted boilerplate stuff from index.tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "darkGray-5": '#141416',
+        "darkGray-4": '#222529',
+        "darkGray-3": '#23262F',
+        "darkGray-2": '#282832',
+        "darkGray-1": '#3B3E44',
+        "gray":'#4A4E54',
+        "lightGray-1": '#84878B',
+        "lightGray-2": '#92929A',
+        "lightGray-2": '#B1B5C3',
+        "lightGray-3": '#E7ECF3',
+        "lightGray-4": '#FCFCFD',
+        "lightGray-5": '#FAFAFA',
+        "transparentDark": '#14141633',
+        "redPrimary": '#FF543D',
+        "whitish": '#FCFCFD',
+        "blueMain": '#316BFF',
+        "gold": '#FFD166',
+      }
+    },
   },
   plugins: [],
 }
