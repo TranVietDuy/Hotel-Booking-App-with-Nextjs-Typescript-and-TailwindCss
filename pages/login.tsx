bc521f793a46fa6b567255d833996543fea78d03
Anton Sydorenko <an.sydorenko@gmail.com>
2022-09-22 21:34:20 +0300

    added dark mode and react forms functionality

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,36 @@
 import React from "react";
-import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
 import { RiFacebookFill } from "react-icons/ri";
+import { useState } from "react";
 
 function LoginForm() {
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [passwordIsVisible, togglePasswordVisibility] = useState(false);
+
+    function handleChange(e: any) {
+        setLoginData((current) => {
+            return {
+                ...current,
+                [e.target.name]: e.target.value,
+            };
+        });
+    }
+
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        console.log("handleSubmit():");
+        console.log(loginData);
+    }
+
     return (
-        <div className="mx-auto space-y-4 bg-whitish-2 py-8 px-10 md:w-7/12 xl:w-1/3">
-            <h2 className="text-center text-5xl font-bold text-darkGray-5 sm:whitespace-nowrap">
+        <div className="mx-auto space-y-4 bg-whitish-2 py-8 px-10 dark:bg-darkGray-5 md:w-7/12 xl:w-1/3">
+            <h2 className="text-center text-5xl font-bold text-darkGray-5 dark:text-lightGray-5 sm:whitespace-nowrap">
                 Welcome Back!
             </h2>
             <div className="flex">
@@ -19,33 +42,53 @@ function LoginForm() {
                     <RiFacebookFill className="inline-block h-8 w-8" />
                 </button>
             </div>
-            <div className="flex items-center justify-around space-x-1 text-sm text-lightGray-1">
+            <div className="flex items-center justify-around space-x-1 text-sm text-lightGray-1 dark:text-lightGray-3">
                 <hr className="inline-block w-1/4 sm:w-1/3" />
                 <span className="whitespace-nowrap">or continue with</span>
                 <hr className="inline-block w-1/4 sm:w-1/3" />
             </div>
-            <div className="w-full text-sm text-darkGray-4">
+            <div className="w-full text-sm text-darkGray-4 dark:text-lightGray-3">
                 <label className="block " htmlFor="email">
                     Email Address
                 </label>
                 <input
-                    className="mt-3 w-full rounded-lg bg-lightGray-3 py-4 px-4"
+                    className="mt-3 w-full rounded-lg bg-lightGray-3 py-4 px-4 dark:bg-darkGray-4 dark:text-lightGray-3"
                     name="email"
                     type="email"
                     placeholder="Enter your email"
+                    value={loginData.email}
+                    onChange={(e) => handleChange(e)}
                 />
             </div>
-            <div className="text-sm text-darkGray-4">
+            <div className="text-sm text-darkGray-4 dark:text-lightGray-3">
                 <label className="block" htmlFor="password">
                     Password
                 </label>
                 <div className="relative w-full">
                     <input
-                        className="mt-3 w-full rounded-lg bg-lightGray-3 py-4 pl-4 pr-8"
-                        type="password"
+                        autoComplete="current-password"
+                        className="mt-3 w-full rounded-lg bg-lightGray-3 py-4 pl-4 pr-8 dark:bg-darkGray-4 dark:text-lightGray-3"
+                        type={passwordIsVisible ? "text" : "password"}
                         placeholder="Enter your password"
+                        name="password"
+                        value={loginData.password}
+                        onChange={(e) => handleChange(e)}
                     />
-                    <AiOutlineEye className="absolute right-2 bottom-3 h-6 w-6" />
+                    {passwordIsVisible ? (
+                        <AiOutlineEyeInvisible
+                            className="absolute right-2 bottom-3 h-6 w-6"
+                            onClick={() =>
+                                togglePasswordVisibility(!passwordIsVisible)
+                            }
+                        />
+                    ) : (
+                        <AiOutlineEye
+                            className="absolute right-2 bottom-3 h-6 w-6"
+                            onClick={() =>
+                                togglePasswordVisibility(!passwordIsVisible)
+                            }
+                        />
+                    )}
                 </div>
                 <div className="flex w-full justify-end py-4">
                     <Link href="/reset-password">
@@ -55,10 +98,15 @@ function LoginForm() {
                     </Link>
                 </div>
                 <div>
-                    <button className="modalButton font-bold">Sign In</button>
+                    <button
+                        onClick={(e) => handleSubmit(e)}
+                        className="modalButton font-bold"
+                    >
+                        Sign In
+                    </button>
                 </div>
             </div>
-            <p className="text-center text-sm font-normal text-darkGray-1">
+            <p className="text-center text-sm font-normal text-darkGray-1 dark:text-lightGray-3">
                 <span>Don't have an account?</span>
                 <Link href="/signup">
                     <a className="text-blueMain"> Sign Up</a>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,13 +1,45 @@
 import React from "react";
-import { AiOutlineEye } from "react-icons/ai";
+import { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
 import { RiFacebookFill } from "react-icons/ri";
 
 function SignupForm() {
+    const [signupData, setSignupData] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        consent: false,
+    });
+
+    const [passwordIsVisible, togglePasswordVisibility] = useState(false);
+
+    function handleChange(e: any) {
+        setSignupData((current) => {
+            if (e.target.type == "checkbox") {
+                return {
+                    ...current,
+                    consent: !current.consent,
+                };
+            }
+            return {
+                ...current,
+                [e.target.name]: e.target.value,
+            };
+        });
+    }
+
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        console.log(`handleSubmit():`);
+        console.dir(signupData);
+    }
+
     return (
-        <div className="mx-auto space-y-4 bg-whitish-2 py-8 px-10 md:w-7/12 xl:w-1/3">
-            <h2 className="text-center text-5xl font-bold text-darkGray-5 sm:whitespace-nowrap">
+        <div className="mx-auto space-y-4 bg-whitish-2 py-8 px-10 dark:bg-darkGray-5 md:w-7/12 xl:w-1/3">
+            <h2 className="text-center text-5xl font-bold text-darkGray-5 dark:text-lightGray-5 sm:whitespace-nowrap">
                 Let's Go
             </h2>
             <div className="flex">
@@ -19,13 +51,14 @@ function SignupForm() {
                     <RiFacebookFill className="inline-block h-8 w-8" />
                 </button>
             </div>
-            <div className="flex items-center justify-around space-x-1 text-sm text-lightGray-1">
+            <div className="flex items-center justify-around space-x-1 text-sm text-lightGray-1 dark:text-lightGray-3">
                 <hr className="inline-block w-1/4 sm:w-1/3" />
                 <span className="whitespace-nowrap">or continue with</span>
                 <hr className="inline-block w-1/4 sm:w-1/3" />
             </div>
-            <div className="flex flex-col space-y-4 sm:flex-row sm:justify-between sm:space-y-0 sm:space-x-4">
-                <div className="text-sm text-darkGray-4 sm:w-1/2">
+
+            <div className="flex flex-col space-y-4 text-darkGray-4 dark:text-lightGray-3 sm:flex-row sm:justify-between sm:space-y-0 sm:space-x-4">
+                <div className="text-sm sm:w-1/2">
                     <label className="block " htmlFor="firstname">
                         First Name
                     </label>
@@ -34,9 +67,11 @@ function SignupForm() {
                         name="firstname"
                         type="text"
                         placeholder="First Name"
+                        value={signupData.firstname}
+                        onChange={(e) => handleChange(e)}
                     />
                 </div>
-                <div className="text-sm  text-darkGray-4 sm:w-1/2">
+                <div className="text-sm sm:w-1/2">
                     <label className="block " htmlFor="lastname">
                         Last Name
                     </label>
@@ -45,10 +80,12 @@ function SignupForm() {
                         name="lastname"
                         type="text"
                         placeholder="Last Name"
+                        value={signupData.lastname}
+                        onChange={(e) => handleChange(e)}
                     />
                 </div>
             </div>
-            <div className="w-full text-sm text-darkGray-4">
+            <div className="w-full text-sm text-darkGray-4 dark:text-lightGray-3">
                 <label className="block " htmlFor="email">
                     Email Address
                 </label>
@@ -57,19 +94,39 @@ function SignupForm() {
                     name="email"
                     type="email"
                     placeholder="Enter your email"
+                    value={signupData.email}
+                    onChange={(e) => handleChange(e)}
                 />
             </div>
-            <div className="text-sm text-darkGray-4">
+            <div className="text-sm text-darkGray-4 dark:text-lightGray-3">
                 <label className="block" htmlFor="password">
                     Password
                 </label>
                 <div className="relative w-full">
                     <input
+                        autoComplete="new-password"
                         className="mt-3 w-full rounded-lg bg-lightGray-3 py-4 pl-4 pr-8"
-                        type="password"
+                        type={passwordIsVisible ? "text" : "password"}
+                        name="password"
                         placeholder="Enter your password"
+                        value={signupData.password}
+                        onChange={(e) => handleChange(e)}
                     />
-                    <AiOutlineEye className="absolute right-2 bottom-3 h-6 w-6" />
+                    {passwordIsVisible ? (
+                        <AiOutlineEyeInvisible
+                            className="absolute right-2 bottom-3 h-6 w-6"
+                            onClick={() =>
+                                togglePasswordVisibility(!passwordIsVisible)
+                            }
+                        />
+                    ) : (
+                        <AiOutlineEye
+                            className="absolute right-2 bottom-3 h-6 w-6"
+                            onClick={() =>
+                                togglePasswordVisibility(!passwordIsVisible)
+                            }
+                        />
+                    )}
                 </div>
                 <div className="flex w-full justify-end py-4">
                     <Link href="/reset-password">
@@ -80,13 +137,16 @@ function SignupForm() {
                 </div>
                 <div className="w-2/3">
                     <label
-                        className="grid grid-cols-[1.5em_max-content] gap-2 hover:cursor-pointer"
+                        className="grid grid-cols-[1.5em_max-content] grid-rows-2 gap-2 hover:cursor-pointer"
                         htmlFor="legalConsent"
                     >
                         <input
-                            className="required appearance-none rounded-sm border-lightGray-1 bg-lightGray-3 checked:border-4 checked:border-lightGray-3 checked:bg-blueMain hover:cursor-pointer"
+                            className="required appearance-none rounded-sm border-lightGray-1 bg-lightGray-3 text-sm checked:border-4 checked:border-lightGray-3 checked:bg-blueMain hover:cursor-pointer"
                             type="checkbox"
                             id="legalConsent"
+                            name="consent"
+                            checked={signupData.consent}
+                            onChange={(e) => handleChange(e)}
                         />
                         <span>
                             I’ve read and accepted
@@ -95,18 +155,26 @@ function SignupForm() {
                                     {` Terms of Service `}
                                 </a>
                             </Link>
+                        </span>
+                        <div></div>
+                        <div>
                             and
                             <Link href="#">
                                 <a className="text-blueMain"> Privacy Policy</a>
                             </Link>
-                        </span>
+                        </div>
                     </label>
                 </div>
                 <div className="my-5">
-                    <button className="modalButton font-bold">Sign Up</button>
+                    <button
+                        className="modalButton font-bold"
+                        onClick={(e) => handleSubmit(e)}
+                    >
+                        Sign Up
+                    </button>
                 </div>
             </div>
-            <p className="text-center text-sm font-normal text-darkGray-1">
+            <p className="text-center text-sm font-normal text-darkGray-1 dark:text-lightGray-2">
                 <span>Already have an account?</span>
                 <Link href="/login">
                     <a className="text-blueMain"> Log In</a>
